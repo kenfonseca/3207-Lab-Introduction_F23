e60f1a58871348afc370817ddd36f0ba43a54aaa
Kenneth <tuo64805@temple.edu>
2023-08-31 22:32:55 -0400

    Removed random.h and added random.c

--- random.c
+++ random.c
@@ -1,12 +1,7 @@
-#ifndef RANDOM
-#define RANDOM
-
+#include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 char randchar(){
 	char randLetter = 65 + (rand() % 25);
 	return randLetter;
 }
-
-#endif
